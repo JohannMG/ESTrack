4d9e7ae5775f866a7f8ec004b2d371c5de9e7cca
JohannMG <hello@johannmg.com>
2015-05-17 23:03:09 -0400

    change Procfile

--- maketable.js
+++
@@ -1,7 +0,0 @@
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || "postgres://postgres:DaVinci@localhost:5432";
-
-var client = new pg.Client(connectionString);
-client.connect();
-console.log(client);
-
