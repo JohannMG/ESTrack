390c0551c6a95e02c16f791872026088457bcb56
JohannMG <hello@johannmg.com>
2015-07-16 17:57:36 -0400

    no new procfile, example dailyEmail

--- DailyEmailTask.js
+++ DailyEmailTask.js
@@ -1,6 +1,12 @@
 //handlebars
 var handlebars = require('handlebars');
 var fs = require('fs');
+var pg = require('pg');
+
+var dbTable =  process.env.usertable || "test_users";
+var dbURL = process.env.DATABASE_URL || "postgres://localhost:5432/test_development";
+
+
 
 var emailData = {
 	thing: "THIS THING!!",
@@ -37,6 +43,31 @@ fs.readFile('./views/dailyEmail.handlebars', 'utf-8', function(err, source){
 	html = emailTemplate(emailData);
 	sendEmail(); 
 }); 
+
+
+
+function getData(){
+	
+	function checkUserInTable(_esid, _location, _room, client){
+	
+	var selectstring = "SELECT * FROM " + dbTable + " WHERE esid=$1";
+	client.query(selectstring, [_esid],
+		function selectUserCallback (err, result) {
+			if (err) { console.log('trouble w check user in table '); console.error(err); }
+	
+			if (result && result.rows.length > 0) {
+				ESID_CACHE[_esid] = Date.now();
+				updateExsitingUser(_location, _room, _esid);
+				return true;
+			}
+	});
+	return false; 
+}
+	
+}
+
+
+
 
 function sendEmail() {
 	console.log(html); 
