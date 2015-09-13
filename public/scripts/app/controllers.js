7f839c1b4ee9931afeaf0135a20e4991a92da2c2
JohannMG <hello@johannmg.com>
2015-09-13 19:42:37 -0400

    working on dashboard

--- public/scripts/app/controllers.js
+++ public/scripts/app/controllers.js
@@ -5,15 +5,51 @@
 	angular.module('mgmtApp').controller('DashboardCtrl', DashboardCtrl);
 	DashboardCtrl.$inject = ['$scope','$location'];
 	function DashboardCtrl($scope, $location){
-		$scope.name = "World";
-	}
+		
+		$scope.wideMetrics = {
+			totalUsers: 12902,
+			users7Days: 102, 
+			usersToday: 66
+		};
+		
+		$scope.locations = [
+			{
+				name: "Orlando", 
+				roomCount: 15, 
+				userRecords: 1200,
+				lastUser: 1442182805598,
+				visitsToday: 20, 
+				visitsYesterday: 130,
+				visits7Days: 1000, 
+				
+			},
+			{
+				name: "Gatlinburg", 
+				roomCount: 22, 
+				userRecords: 1800,
+				lastUser: 1442182882860,
+				visitsToday: 80, 
+				visitsYesterday: 250,
+				visits7Days: 5400, 
+				
+			}
+		]
+	}//end DashboardCtrl
+
+
 
 	/*  ReportsCtrl ------------------ */
 	angular.module('mgmtApp').controller('ReportsCtrl', ReportsCtrl);
+	
 	ReportsCtrl.$inject = ['$scope','$location'];
 	function ReportsCtrl($scope, $location){
+		
 		$scope.bignum = 1267;
-	}
+		
+	}//end ReportsCtrl
+	
+	
+	
 	
 	/*  navCtrl ------------------
 		Controller for main navigation menu on all pages 
@@ -21,10 +57,12 @@
 	angular.module('mgmtApp').controller('navCtrl', navCtrl);
 	navCtrl.$inject = ['$scope','$location'];
 	function navCtrl ($scope, $location){
+		
 		$scope.activeLink = function(linkPath){
 			var currentPath = $location.path();
 			return (currentPath === linkPath) ? "activePage" : "";
 		}
-	};
+		
+	}; //end navCtrl
 	
 }());
